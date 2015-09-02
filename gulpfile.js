e0f467f34d4e6bbf3f160b60ddcd4b0e1b2c149b
Tom Knapen <tom.knapen@outlook.com>
2015-09-02 23:41:28 +0200

    sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('hello', function() {
-	console.log('Hello world!');
+gulp.task('sass', function() {
+	gulp.src('source-files')
+		.pipe(sass())
+		.pipe(gulp.dest('destination'))
 });
