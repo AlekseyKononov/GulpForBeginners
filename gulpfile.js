0163ac40a455da83510df5e812f9c282cfe6c8b9
Tom Knapen <tom.knapen@outlook.com>
2015-09-03 16:31:44 +0200

    sass watcher

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,9 @@ var sass = require('gulp-sass');
 gulp.task('sass', function() {
 	gulp.src('app/sass/**/*.scss')
 		.pipe(sass())
-		.pipe(gulp.dest('app/css'))
+		.pipe(gulp.dest('app/css'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('app/sass/**/*.scss', ['sass']);
 });
