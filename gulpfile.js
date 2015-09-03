16bde85929afd4fe9b5155b5420b67e75e23250c
Tom Knapen <tom.knapen@outlook.com>
2015-09-03 15:33:49 +0200

    pattern matching

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-	gulp.src('app/sass/styles.scss')
+	gulp.src('app/sass/**/*.scss')
 		.pipe(sass())
 		.pipe(gulp.dest('app/css'))
 });
